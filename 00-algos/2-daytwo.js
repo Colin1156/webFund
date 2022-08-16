852d670a08f70ac27f51c4621965692480955344
Colin1156 <clyons1156@gmail.com>
2022-08-16 17:28:21 -0400

    002pm

---
+++ 00-algos/2-daytwo.js
@@ -0,0 +1,33 @@
+var amazingShow1 = "Curb your enthusiam"; //This is a variable = to a string (something in quotation marks)
+var magicNumber = 23; //this is an number not necessarily an integer
+
+amazingShow1 = "The office"; //this is an example of reassigning the variable
+
+var isRobSpeakingFactsAboutBasketball = false; //boolean is either true or false
+
+
+// a data type is a simple piece of information
+
+//a data structure is something that can sotre multiple data types (arrays, objects(hash maps) trees, linked lists, etc)
+
+var greatestArtists = ["Drake", "J Cole", "Taylor Swift"]
+//index (positions)        0        1           2    
+//the actual values you see in the array are called: Elements
+
+var randomNums = [23, 0, 77, 12, 8]
+
+var mixedArry = ["Potato", 5, false, true, "hello", 9]
+
+console.log("hi")
+console.log(greatestArtists)
+// console.log(greatestArtists[0])
+// console.log(greatestArtists[1])
+// console.log(greatestArtists[2])
+// console.log(greatestArtists[3])
+// console.log(greatestArtists[4])
+
+//a loop is used to do something repeatedly, Example- print every number from 1 to 100
+ for(var num =1; num<=100; num++){
+    //this is the body of the for loop
+    console.log(num);
+ }
